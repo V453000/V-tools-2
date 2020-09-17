56314200208943eb3d7ed61294e4799ec79e3319
Vaclav Benc <vaclav.benc@>
2020-09-17 14:47:23 +0200

    Moved Generate Render Nodes Panel into the same .py file

--- Vtools2_generate_render_nodes.py
+++ Vtools2_generate_render_nodes.py
@@ -1,5 +1,18 @@
 import bpy
 
+class Vtools2_generate_render_nodes_Panel(bpy.types.Panel):
+    bl_idname = 'generate_render_nodes_panel'
+    bl_label = 'V-Tools-2 Panel'
+    bl_category = 'V-Tools-2'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.operator('scene.generate_render_nodes', text = 'Generate Render Nodes', icon = 'NODETREE')
+
 class Vtools2_generate_render_nodes_Operator(bpy.types.Operator):
     bl_idname = 'scene.generate_render_nodes'
     bl_label = 'Generate Render Nodes'
