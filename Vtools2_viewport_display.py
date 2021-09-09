f6eee7900c153ae753a4e78061e542a8507d118c
V453000 <vaclav.benc@gmail.com>
2021-09-09 16:03:42 +0200

    Viewport display doesn't complain when you don't give it any objects.

--- Vtools2_viewport_display.py
+++ Vtools2_viewport_display.py
@@ -166,80 +166,81 @@ class VTOOLS2_OT_viewport_display(bpy.types.Operator):
             self.show_bounds = show_bounds
             self.display_bounds_type = display_bounds_type
 
-        original_active_data = object_data(
-                      active_object.show_name,
-                      active_object.show_axis,
-                      active_object.show_all_edges,
-                      active_object.show_texture_space,
-                      active_object.display.show_shadows,
-                      active_object.show_in_front,
-                      active_object.color,
-                      active_object.display_type,
-                      active_object.show_bounds,
-                      active_object.display_bounds_type,
-                      )
+        if active_object is not None:
+          original_active_data = object_data(
+                        active_object.show_name,
+                        active_object.show_axis,
+                        active_object.show_all_edges,
+                        active_object.show_texture_space,
+                        active_object.display.show_shadows,
+                        active_object.show_in_front,
+                        active_object.color,
+                        active_object.display_type,
+                        active_object.show_bounds,
+                        active_object.display_bounds_type,
+                        )
 
-        for obj in bpy.context.selected_objects:
+          for obj in bpy.context.selected_objects:
 
-          if self.show_name != 'Unchanged':
-            if self.show_name != 'Copy Active':
-              obj.show_name = to_bool(self.show_name)
-            else:
-              obj.show_name = original_active_data.show_name
+            if self.show_name != 'Unchanged':
+              if self.show_name != 'Copy Active':
+                obj.show_name = to_bool(self.show_name)
+              else:
+                obj.show_name = original_active_data.show_name
 
-          if self.show_axis != 'Unchanged':
-            if self.show_axis != 'Copy Active':
-              obj.show_axis = to_bool(self.show_axis)
-            else:
-              obj.show_axis = original_active_data.show_axis
+            if self.show_axis != 'Unchanged':
+              if self.show_axis != 'Copy Active':
+                obj.show_axis = to_bool(self.show_axis)
+              else:
+                obj.show_axis = original_active_data.show_axis
 
-          if self.show_all_edges != 'Unchanged':
-            if self.show_all_edges != 'Copy Active':
-              obj.show_all_edges = to_bool(self.show_all_edges)
-            else:
-              obj.show_all_edges = original_active_data.show_all_edges
+            if self.show_all_edges != 'Unchanged':
+              if self.show_all_edges != 'Copy Active':
+                obj.show_all_edges = to_bool(self.show_all_edges)
+              else:
+                obj.show_all_edges = original_active_data.show_all_edges
 
-          if self.show_texture_space != 'Unchanged':
-            if self.show_texture_space != 'Copy Active':
-              obj.show_texture_space = to_bool(self.show_texture_space)
-            else:
-              obj.show_texture_space = original_active_data.show_texture_space
-          
-          if self.show_shadows != 'Unchanged':
-            if self.show_shadows != 'Copy Active':
-              obj.display.show_shadows = to_bool(self.show_shadows)
-            else:
-              obj.display.show_shadows = original_active_data.show_shadows
+            if self.show_texture_space != 'Unchanged':
+              if self.show_texture_space != 'Copy Active':
+                obj.show_texture_space = to_bool(self.show_texture_space)
+              else:
+                obj.show_texture_space = original_active_data.show_texture_space
+            
+            if self.show_shadows != 'Unchanged':
+              if self.show_shadows != 'Copy Active':
+                obj.display.show_shadows = to_bool(self.show_shadows)
+              else:
+                obj.display.show_shadows = original_active_data.show_shadows
 
-          if self.show_in_front != 'Unchanged':
-            if self.show_in_front != 'Copy Active':
-              obj.show_in_front = to_bool(self.show_in_front)
-            else:
-              obj.show_in_front = original_active_data.show_in_front
+            if self.show_in_front != 'Unchanged':
+              if self.show_in_front != 'Copy Active':
+                obj.show_in_front = to_bool(self.show_in_front)
+              else:
+                obj.show_in_front = original_active_data.show_in_front
 
-          if self.show_bounds != 'Unchanged':
-            if self.show_bounds != 'Copy Active':
-              obj.show_bounds = to_bool(self.show_bounds)
-            else:
-              obj.show_bounds = original_active_data.show_bounds
+            if self.show_bounds != 'Unchanged':
+              if self.show_bounds != 'Copy Active':
+                obj.show_bounds = to_bool(self.show_bounds)
+              else:
+                obj.show_bounds = original_active_data.show_bounds
 
-          if self.display_type != 'Unchanged':
-            if self.display_type != 'Copy Active':
-              obj.display_type = self.display_type
-            else:
-              obj.display_type = original_active_data.display_type
+            if self.display_type != 'Unchanged':
+              if self.display_type != 'Copy Active':
+                obj.display_type = self.display_type
+              else:
+                obj.display_type = original_active_data.display_type
 
-          if self.display_bounds_type != 'Unchanged':
-            if self.display_bounds_type != 'Copy Active':
-              obj.display_bounds_type = self.display_bounds_type
-            else:
-              obj.coldisplay_bounds_typeor = original_active_data.display_bounds_type
+            if self.display_bounds_type != 'Unchanged':
+              if self.display_bounds_type != 'Copy Active':
+                obj.display_bounds_type = self.display_bounds_type
+              else:
+                obj.coldisplay_bounds_typeor = original_active_data.display_bounds_type
 
-          if self.color_use != 'Unchanged':
-            if self.color_use != 'Copy Active':
-              obj.color = self.color
-            else:
-              obj.color = original_active_data.color
+            if self.color_use != 'Unchanged':
+              if self.color_use != 'Copy Active':
+                obj.color = self.color
+              else:
+                obj.color = original_active_data.color
 
         return {'FINISHED'}
 
