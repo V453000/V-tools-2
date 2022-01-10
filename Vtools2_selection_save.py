234de9e7b159592fd2e9fa4e12aebe8c0c09f538
V453000 <vaclav.benc@gmail.com>
2022-01-10 14:35:16 +0100

    Fixed?

--- Vtools2_selection_save.py
+++ Vtools2_selection_save.py
@@ -28,7 +28,7 @@ class VTOOLS2_OT_selection_save(bpy.types.Operator):
         a = context.scene.saved_active.add()
         a.name = active_object.name
 
-        return ('FINISHED')
+        return {'FINISHED'}
 
 
 class VTOOLS2_OT_selection_load(bpy.types.Operator):
@@ -37,10 +37,27 @@ class VTOOLS2_OT_selection_load(bpy.types.Operator):
     bl_label = 'Load Selection'
     bl_options = {'REGISTER', 'UNDO'}
 
+    add_to_selection : bpy.props.BoolProperty(
+    name = 'Add to selection',
+    default = True
+  )
+
     def execute(self, context):
 
-        for obj in context.scene.saved_selection:
-            print(obj.name)
+        if self.add_to_selection == True:
+            bpy.ops.object.select_all(action = 'DESELECT')
 
-        return ('FINISHED')
+        for obj in context.scene.saved_selection:
+            selected_object_index = bpy.context.scene.objects.find(obj.name)
+            if selected_object_index != -1:
+                context.scene.objects[selected_object_index].select_set(True)
+        
+        active_obj = context.scene.saved_active
+        active_object_index = bpy.context.scene.objects.find(active_obj[0].name)
+        if active_object_index != -1:
+            bpy.context.view_layer.objects.active = bpy.context.scene.objects[active_object_index]
+        else:
+            bpy.context.view_layer.objects.active = None
+            
+        return {'FINISHED'}
         
